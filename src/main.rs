a449f3ce310ece93f12b0ea399a595956a9becf4
zkud <kudirko.anton@gmail.com>
2021-06-29 22:07:49 +0300

    Inited a buffer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-use vulkano::device::{Device, DeviceExtensions, Features};
+use std::sync::Arc;
+use vulkano::buffer::{BufferUsage, CpuAccessibleBuffer};
+use vulkano::device::{Device, DeviceExtensions, Features, Queue};
 use vulkano::instance::{Instance, InstanceExtensions, PhysicalDevice};
 use vulkano::{app_info_from_cargo_toml, Version};
 
 fn main() {
+    let (mut device, mut queue) = init_device();
+
+    let buffer = init_buffer(&device);
+}
+
+fn init_buffer(device: &Arc<Device>) -> Arc<CpuAccessibleBuffer<i32>> {
+    let data: i32 = 123;
+    CpuAccessibleBuffer::from_data(device.clone(), BufferUsage::all(), false, data)
+        .expect("failed to create buffer")
+}
+
+fn init_device() -> (Arc<Device>, Arc<Queue>) {
     let app_infos = app_info_from_cargo_toml!();
     let instance = Instance::new(
         Some(&app_infos),
@@ -11,16 +25,16 @@ fn main() {
         None,
     )
     .unwrap();
-    let physical = PhysicalDevice::enumerate(&instance)
+    let physical_device = PhysicalDevice::enumerate(&instance)
         .next()
         .expect("no device available");
-    let queue_family = physical
+    let queue_family = physical_device
         .queue_families()
         .find(|&q| q.supports_graphics())
         .expect("couldn't find a graphical queue family");
     let (device, mut queues) = {
         Device::new(
-            physical,
+            physical_device,
             &Features::none(),
             &DeviceExtensions::none(),
             [(queue_family, 0.5)].iter().cloned(),
@@ -28,4 +42,6 @@ fn main() {
         .expect("failed to create device")
     };
     let queue = queues.next().unwrap();
+
+    (device, queue)
 }
